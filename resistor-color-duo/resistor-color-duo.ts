8d99911d829442ca41d4ce84cb85850cd0e8fe65
Fermin <josefermin.hernandez@adevinta.com>
2023-01-01 18:10:44 +0100

    exercism resistor-color

---
+++ resistor-color-duo/resistor-color-duo.ts
@@ -0,0 +1,11 @@
+export function decodedValue(resistorValues: string[]): number {
+  const colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+  if (resistorValues.length < 2){
+    throw new Error('At least two colors need to be present')
+  }
+  const firstColor = colors.indexOf(resistorValues[0])
+  const secondColor = colors.indexOf(resistorValues[1])
+
+  return firstColor * 10 + secondColor
+}
